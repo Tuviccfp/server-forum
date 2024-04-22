b906a03c140e5b3b4c04172906ea3afaf62e4d68
Tuviccfp <victorcordeirofp@gmail.com>
2024-04-22 13:38:24 -0300

    first commit and entity user created

---
+++ src/test/java/com/platformtest/app/PlatformTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platformtest.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
