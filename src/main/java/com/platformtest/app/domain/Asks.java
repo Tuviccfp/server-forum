b4cce646ff51594e53ae2a11e8f2b3a52b68082a
Tuviccfp <victorcordeirofp@gmail.com>
2024-05-01 13:23:41 -0300

    retirada do uso do json ignore

--- src/main/java/com/platformtest/app/domain/Asks.java
+++ src/main/java/com/platformtest/app/domain/Asks.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
